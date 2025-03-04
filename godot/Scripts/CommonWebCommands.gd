9b88a813992a743678f83e1a326f64d7b7558977
Johannes Deml <public@deml.io>
2025-03-05 00:09:27 +0100

    Add commands to pause and quit the application

--- godot/Scripts/CommonWebCommands.gd
+++ godot/Scripts/CommonWebCommands.gd
@@ -31,6 +31,30 @@ func _ready():
 		["message: String"]
 	)
 
+	web_commands.register_command(
+		"toggle_pause",
+		toggle_pause,
+		"Toggles the game pause state"
+	)
+
+	web_commands.register_command(
+		"pause_game",
+		pause_game,
+		"Pauses the game"
+	)
+
+	web_commands.register_command(
+		"unpause_game",
+		unpause_game,
+		"Unpauses the game"
+	)
+
+	web_commands.register_command(
+		"quit",
+		quit_game,
+		"Quits the application"
+	)
+
 func set_time_scale(scale: float):
 	Engine.time_scale = scale
 	print("Time scale set to: ", scale)
@@ -47,3 +71,19 @@ func log_example_message():
 	print_rich("This is an [b]example of a [i]rich text[/i] message (print_rich())[/b]. [color=red]It supports[/color] [b]BBCode[/b] [url=https://docs.godotengine.org/en/stable/classes/class_%40globalscope.html#class-globalscope-method-print-rich]like this[/url].")
 	push_warning("This is a warning message (push_warning())")
 	push_error("This is an error message (push_error())")
+
+func toggle_pause():
+	get_tree().paused = !get_tree().paused
+	print("Game pause state: ", "Paused" if get_tree().paused else "Unpaused")
+
+func pause_game():
+	get_tree().paused = true
+	print("Game paused")
+
+func unpause_game():
+	get_tree().paused = false
+	print("Game unpaused")
+
+func quit_game():
+	print("Quitting application...")
+	get_tree().quit()
