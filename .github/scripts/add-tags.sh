6283e96e48ac04a2ca31be702f7758398de4662e
Johannes Deml <public@deml.io>
2025-03-16 11:39:55 +0100

    Fix tag name

--- .github/scripts/add-tags.sh
+++ .github/scripts/add-tags.sh
@@ -8,7 +8,7 @@ GODOT_VERSION=$1
 echo "Running add_tags.sh with GODOT_VERSION: $GODOT_VERSION"
 
 # Create tags for different configurations
-git tag -a -f godot-$GODOT_VERSION -m "[Automated workflow] Created by upgrade-godot"
-git tag -a -f godot-$GODOT_VERSION-debug -m "[Automated workflow] Created by upgrade-godot"
+git tag -a -f $GODOT_VERSION -m "[Automated workflow] Created by upgrade-godot"
+git tag -a -f $GODOT_VERSION-debug -m "[Automated workflow] Created by upgrade-godot"
 # Push tags
 git push origin -f --tags
